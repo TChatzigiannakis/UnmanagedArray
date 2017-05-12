44a70ddac349381345f7e1d543970b55127166d5
TChatzigiannakis <tchatzigiannakis@gmail.com>
2017-05-12 23:01:08 +0300

    Added reference to ValueTuple.

---
+++ Tests/Common.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal static class Common
+    {
+        public static (long time, T result) Measure<T>(this Stopwatch stopwatch, Func<T> act)
+        {
+            stopwatch.Restart();
+            var result = act();
+            stopwatch.Stop();
+            return (stopwatch.ElapsedMilliseconds, result);
+        }
+    }
+}
